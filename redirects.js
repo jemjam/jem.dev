30f81ed1b2406c23629b6832f5005570900ccb94
Jem (Jam) Bezooyen <hey@jem.dev>
2020-12-04 16:04:37 -0800

    feat(redirect): For cascadia.js 2020 notes
    I know I shared some links out for the notes to cascadia.js 2020 notes,
    so this is one redirect to definitely include
    
    Also includes some jsdoc style typedefs to improve redirect DX.
    
    This is probably enough to merge this branch...

--- redirects.js
+++ redirects.js
@@ -1,3 +1,13 @@
+/**
+ * @typedef NextRedirect
+ * @property {string} source Redirecting FROM this path
+ * @property {string} destination Redirecting TO this path
+ * @property {boolean} permanent Is this redirect permanent?
+ */
+/**
+ * Set path redirects, see https://nextjs.org/docs/api-reference/next.config.js/redirects
+ * @type Array<NextRedirect>
+ */
 const redirects = [
     {
         source: '/100days/:slug*',
@@ -5,6 +15,11 @@ const redirects = [
         permanent: true,
     },
     { source: '/whois', destination: '/about', permanent: true },
+    {
+        source: '/conf/cascadiaja/:rest*',
+        destination: '/notes/conf/cascadiajs/:rest*',
+        permanent: true,
+    },
 ]
 
 module.exports = async () => {
