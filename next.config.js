f80a2f7686c978f455102725f2de9546a74c621b
Jem (Jam) Bezooyen <hey@jem.dev>
2020-12-04 16:41:00 -0800

    refactor: Shift to next-plugin-preact
    Previously the next.config.js had to have some values overridden for
    preact-compatibility. With the plugin, much of this can be removed.
    
    Also clean out a few unused imports, gradually whittling away old config
    that is no longer in use

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
+const withPreact = require('next-plugin-preact');
 const process = require('process')
 const images = require('remark-images')
 const emoji = require('remark-emoji')
 const highlight = require('remark-highlight.js')
-const withMdxEnhanced = require('next-mdx-enhanced')
-const redirects = require('./redirects')
 
+const redirects = require('./redirects')
 
 // Standard (non-enhanced) mdx approach...
 const withMDX = require('@next/mdx')({
@@ -15,45 +15,11 @@ const withMDX = require('@next/mdx')({
     }
 })
 
-// For now we're using enchance layouts tho...
-// const withMdx = withMdxEnhanced({
-//     layoutPath: 'layouts',
-//     // defaultLayout: true,
-//     fileExtensions: ['mdx'],
-//     remarkPlugins: [images, emoji, highlight],
-//     rehypePlugins: [],
-//     extendFrontMatter: {
-//         process: (mdxContent, frontMatter) => {},
-//         phase: 'prebuild|loader|both',
-//     },
-// })
-
-module.exports = withMDX({
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        // Note: we provide webpack above so you should not `require` it
-        // Perform customizations to webpack config
-        // Important: return the modified config
-        config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
-        preactAliases = {
-            react: 'preact/compat',
-            'react-dom': 'preact/compat',
-            'react-ssr-prepass': 'preact-ssr-prepass',
-        }
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            ...preactAliases,
-        }
-        return config
-    },
-    webpackDevMiddleware: (config) => {
-        // Perform customizations to webpack dev middleware config
-        // Important: return the modified config
-        return config
-    },
+module.exports = withPreact(withMDX({
     pageExtensions: ['js', 'jsx', 'mdx', 'tsx', 'ts'],
     env: {
         NEXT_PUBLIC_ANALYTICS_SITE_ID: process.env.FATHOM_SITE_ID,
         NEXT_PUBLIC_ANALYTICS_URL: process.env.FATHOM_URL,
     },
     redirects,
-})
+}))
